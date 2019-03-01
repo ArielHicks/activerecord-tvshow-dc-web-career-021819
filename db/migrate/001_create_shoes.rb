b2dfcfe1455d0e3ac93803d40780488220e4fb51
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-01 15:29:40 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shoes.rb
+++ db/migrate/001_create_shoes.rb
@@ -1,4 +1,4 @@
-class CreateShows < ActiveRecord::Migration
+class CreateShoes < ActiveRecord::Migration
 
   def change
   create_table :shows do |t|
@@ -8,5 +8,5 @@ class CreateShows < ActiveRecord::Migration
     t.string :network
     t.integer :rating
    end
- end 
+ end
 end
