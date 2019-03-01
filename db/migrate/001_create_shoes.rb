40935c22fc99825717627dc81c227c376a5a5a59
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-01 15:25:53 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shoes.rb
+++ db/migrate/001_create_shoes.rb
@@ -1,7 +1,11 @@
 class CreateShows < ActiveRecord::Migration
 
   def change
-
-  end
+  create_table :shows do |t|
+    #primary key of :id is created for us!
+    t.string :name
+    t.string :day
+    t.string :network
+    t.integer :rating  end
 
 end
